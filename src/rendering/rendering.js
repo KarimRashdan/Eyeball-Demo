693527c2e14c5764df6f663d32e5a85bf0b44d7c
Kream545 <kr749@bath.ac.uk>
2025-11-30 17:47:04 +0000

    push 3

--- src/rendering/rendering.js
+++ src/rendering/rendering.js
@@ -1,2 +1,12 @@
 import * as THREE from "https://unpkg.com/three@0.181.2/build/three.module.js";
 
+let scene, camera, renderer, eyeball;
+
+// https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_on_the_web/Building_up_a_basic_demo_with_Three.js
+export function initRendering(canvas) {
+
+    // create renderer and attach it to canvas
+    renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
