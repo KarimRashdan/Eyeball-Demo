92c4e4a9bfaf3a597f6adea582ae4f698dbdf5d0
Kream545 <kr749@bath.ac.uk>
2025-11-30 17:13:02 +0000

    push 3

---
+++ src/behaviour/behaviour.js
@@ -0,0 +1,7 @@
+// Responsible for initializing eyeball's emotional state
+export function initBehaviour() {
+    console.log("Behaviour initialized");
+}
+
+export function updateBehaviour() {
+}
---
+++ src/tracking/tracking.js
@@ -0,0 +1,9 @@
+// Responsible for initializing everything needed for the tracking (i.e. enabling webcam, setting up mediapipe, etc.)
+export function initTracking() {
+    console.log("Tracking initialized");
+}
+
+// Responsible for returning the target for the eyeball to track
+export function getTargets() {
+    return [];
+}
