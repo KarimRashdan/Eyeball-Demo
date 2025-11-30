2f78c53cf3d14a029e0cd15b06fc69dbae2558ef
Kream545 <kr749@bath.ac.uk>
2025-11-30 18:40:18 +0000

    push 3

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { initRendering, updateRendering} from "./src/rendering/rendering.js";
-import { initTracking, getTargets } from "./src/tracking/tracking.js";
-import { initBehaviour, updateBehaviour } from "./src/behaviour/behaviour.js";
-import { initUI, updateUI } from "./src/ui/ui.js";
+import { initRendering, updateRendering} from "./rendering/rendering.js";
+import { initTracking, getTargets } from "./tracking/tracking.js";
+import { initBehaviour, updateBehaviour } from "./behaviour/behaviour.js";
+import { initUI, updateUI } from "./ui/ui.js";
 
 let lastTime = 0;
 
