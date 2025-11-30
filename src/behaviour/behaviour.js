f9e37bb6f3d05d9d8a1943f6b7f03588887053ef
Kream545 <kr749@bath.ac.uk>
2025-11-30 17:25:25 +0000

    push 3

--- src/behaviour/behaviour.js
+++ src/behaviour/behaviour.js
@@ -1,7 +1,14 @@
-// Responsible for initializing eyeball's emotional state
+// responsible for initializing eyeball's emotional state
 export function initBehaviour() {
     console.log("Behaviour initialized");
 }
 
+// updates the eyeball's emotional state, choose target, not entirely sure yet
 export function updateBehaviour() {
+    // Logic for returns
+    return {
+        mode: "placeholder: behaviour mode",
+        targetCoords: null,
+        emotion: "placeholder: behaviour emotion",
+    };
 }
--- src/ui/ui.js
+++ src/ui/ui.js
@@ -1,2 +1,8 @@
+// initializes the UI components
 export function initUI() {
+}
+
+// 
+export function updateUI(behaviourState) {
+    // prompts e.g. make angry face
 }
